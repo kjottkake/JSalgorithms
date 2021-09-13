633fc181ea35e4564b413265df79a0dab2604225
kjottkake <38797885+kjottkake@users.noreply.github.com>
2021-09-13 13:56:03 +0200

    added selection sort

--- sort.js
+++ sort.js
@@ -9,9 +9,9 @@ function genNumbers(){
     console.table(numbers);
 }
 
-
+//bubblesort
 function bubbleSort(arr){
-    let t0 = performance.now()
+    let t0 = performance.now() //testing speed
     let len = arr.length;
     for (i = len-1; i >=0; i--){
         for (j = 1; j<=i; j++){
@@ -23,10 +23,28 @@ function bubbleSort(arr){
         }
     }
 
-    let t1 = performance.now()
-    console.log("Call to bubbleSort took " + (t1 - t0) + " milliseconds.")
+    let t1 = performance.now() //testing speed
+    console.log("Call to bubbleSort took " + (t1 - t0) + " milliseconds.") //output speed test
     return arr;
 }
 
-
-
+//selection sort
+function selectionSort(arr){
+    let t0 = performance.now() //testing speed
+    var minIdx, temp, 
+        len = arr.length;
+    for(var i = 0; i < len; i++){
+      minIdx = i;
+      for(var  j = i+1; j<len; j++){
+         if(arr[j]<arr[minIdx]){
+            minIdx = j;
+         }
+      }
+      temp = arr[i];
+      arr[i] = arr[minIdx];
+      arr[minIdx] = temp;
+    }
+    let t1 = performance.now() //testing speed
+    console.log("Call to bubbleSort took " + (t1 - t0) + " milliseconds.") //output speed test
+    return arr;
+  }
